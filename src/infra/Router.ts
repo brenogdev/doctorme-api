7cbd2f18ff9649da4e60207beec0cac5ab15e522
Breno Guilherme de Souza Lopes <105818141+brenogdev@users.noreply.github.com>
2024-06-05 23:12:47 -0300

    feat: configure server

---
+++ src/infra/Router.ts
@@ -0,0 +1,26 @@
+import 'express-async-errors'
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+export default class Router {
+  app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(express.json());
+
+    this.setRoutes();
+  }
+
+  private setRoutes() {}
+
+  public start(port: number) {
+    this.app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  }
+}
